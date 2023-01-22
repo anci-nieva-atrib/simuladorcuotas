7843604c73f2e191e0b88500880edee98b71d634
Ana Belen Nieva Atrib <anci.nieva.atrib@gmail.com>
2023-01-22 16:52:51 -0300

    cambios

--- juego.js
+++ juego.js
@@ -20,7 +20,7 @@ if(pista1 == "javascript"){
         alert("La palabra ingresada " + pista1 + " es incorrecta. Intentá nuevamente.");
         pista1 = prompt("Ingresá un lenguaje de programación");
 
-}alert("La palabra ingresada " + pista1 + " es CORRECTA y sumaste " + puntos + " puntos!");
+}
 
 
 
@@ -37,9 +37,9 @@ if(pista2 == "messi"){
         pista2 = prompt("Ingresá el apellido del jugador de la selección Argentina que tiene la camiseta 10");
     }while(pista2 == false)
     alert("La palabra ingresada " + pista2 + " es incorrecta. Intentá nuevamente.");
-    pista2 = prompt("Ingresá un lenguaje de programación");
+    pista2 = prompt("Ingresá el apellido del jugador de la selección Argentina que tiene la camiseta 10");
 
-}alert("La palabra ingresada " + pista2 + " es CORRECTA y sumaste " + puntos + " puntos!");
+}
 
 
 let pista3 = prompt("Ingresá el de copas del mundo que ganó Argentina");
@@ -55,8 +55,8 @@ if(pista3 == "3"){
         pista3 = prompt("Ingresá el de copas del mundo que ganó Argentina");
     }while(pista3 == false)
     alert("La palabra ingresada " + pista3 + " es incorrecta. Intentá nuevamente.");
-    pista3 = prompt("Ingresá un lenguaje de programación");
-}alert("La palabra ingresada " + pista3 + " es CORRECTA y sumaste " + puntos + " puntos!");
+    pista3 = prompt("Ingresá el de copas del mundo que ganó Argentina");
+}
 
 
 
