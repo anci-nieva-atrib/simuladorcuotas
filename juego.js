bdb9feb5a9431ff1e918832d13f4e58bb4abca18
Ana Belen Nieva Atrib <anci.nieva.atrib@gmail.com>
2023-01-22 16:35:16 -0300

    c

--- juego.js
+++ juego.js
@@ -16,10 +16,9 @@ if(pista1 == "javascript"){
     do{
         alert("La palabra ingresada " + pista1 + " es incorrecta. Intentá nuevamente.");
         pista1 = prompt("Ingresá un lenguaje de programación");
-    }while(pista1 == false){
+    }while(pista1 == false)
         alert("La palabra ingresada " + pista1 + " es incorrecta. Intentá nuevamente.");
         pista1 = prompt("Ingresá un lenguaje de programación");
-    } 
 
 }alert("La palabra ingresada " + pista1 + " es CORRECTA y sumaste " + puntos + " puntos!");
 
@@ -36,10 +35,9 @@ if(pista2 == "messi"){
     do{
         alert("La palabra ingresada " + pista2 + " es incorrecta. Intentá nuevamente.");
         pista2 = prompt("Ingresá el apellido del jugador de la selección Argentina que tiene la camiseta 10");
-    }while(pista2 == false){
+    }while(pista2 == false)
     alert("La palabra ingresada " + pista2 + " es incorrecta. Intentá nuevamente.");
     pista2 = prompt("Ingresá un lenguaje de programación");
-    } 
 
 }alert("La palabra ingresada " + pista2 + " es CORRECTA y sumaste " + puntos + " puntos!");
 
@@ -55,10 +53,9 @@ if(pista3 == "3"){
     do{
         alert("La palabra ingresada " + pista3 + " es incorrecta. Intentá nuevamente.");
         pista3 = prompt("Ingresá el de copas del mundo que ganó Argentina");
-    }while(pista3 == false){
+    }while(pista3 == false)
     alert("La palabra ingresada " + pista3 + " es incorrecta. Intentá nuevamente.");
     pista3 = prompt("Ingresá un lenguaje de programación");
-    }   
 }alert("La palabra ingresada " + pista3 + " es CORRECTA y sumaste " + puntos + " puntos!");
 
 
