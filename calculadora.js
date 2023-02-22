dc63f394fe077727946d8bf2c68b8aa5cb8f9199
Ana Belen Nieva Atrib <anci.nieva.atrib@gmail.com>
2023-02-22 20:40:41 -0300

    tercera pre entrega js

--- calculadora.js
+++ calculadora.js
@@ -1,211 +1,335 @@
-let nombre = (prompt("Hola, somos Smartphone Store. ¿Cómo te llamás?") || "").toUpperCase();
-let preguntaMarca = (prompt(`${nombre}, ¿sobre qué marca de smartphone querés consultar? ¿Apple, Samsung o las dos?`, `Apple`) || "").toUpperCase();
-
+//Estos son los smartphones disponibles
 const smartphone = [
     { 
+        id:1,
+        img:"./Multimedia/iphone14.webp",
         marca: "Apple",
         modelo: "Iphone 14",
         memoria: "128 GB",
-        color: "Medianoche",
+        color: "Blanco Estelar",
+        precio: 487999,
     },
     { 
+        id:2,
+        img:"./Multimedia/iphone14pro.webp",
         marca: "Apple",
         modelo: "Iphone 14 Pro",
         memoria: "256 GB",
-        color: "Negro espacial",
+        color: "Morado Oscuro",
+        precio: 704499,
     },
     { 
+        id:3,
+        img:"./Multimedia/s23.webp",
         marca: "Samsung",
         modelo: "S23",
-        memoria: "256 GB",
+        memoria: "128 GB",
         color: "Phantom Black",
+        precio: 299999,
     },
     { 
+        id:4,
+        img:"./Multimedia/s23ultra.webp",
         marca: "Samsung",
         modelo: "S23 Ultra",
         memoria: "256 GB",
-        color: "Cream",
+        color: "Green",
+        precio: 459999,
     },
 ];
 
 
-let preguntaModelo;
 
 
-if (preguntaMarca == "APPLE"){
-    alert("Te mostramos por consola los Iphone disponibles.");
+//Aqui estan las acciones asignadas a cada boton que filtra por marca
+let mostrarProductosApple = document.querySelector("#botonMarcaApple");
+mostrarProductosApple.addEventListener("click", () => {
+    let productosApple = smartphone.filter(producto => producto.marca === "Apple");
+    crearProductos(productosApple);
+    mostrarContenido("#paso2");
+});
 
-    console.log(smartphone[0]);
-    console.log(smartphone[1]);
-    preguntaModelo = (prompt("¿Cuál modelo te gustaría comprar y financiar?", "Iphone 14 Pro") || "").toUpperCase();
+const mostrarProductosSamsung = document.querySelector("#botonMarcaSamsung");
+mostrarProductosSamsung.addEventListener("click", () => {
+    const productosSamsung = smartphone.filter(producto => producto.marca === "Samsung");
+    crearProductos(productosSamsung);
+    mostrarContenido("#paso2");
+});
 
-}else if(preguntaMarca == "SAMSUNG"){
-    alert("Te mostramos por consola los Samsung disponibles.");
+const mostrarProductosTodos = document.querySelector("#botonMarcaLasDos");
+mostrarProductosTodos.addEventListener("click", () => {
+    const todosLosProductos = smartphone;
+    crearProductos(todosLosProductos);
+    mostrarContenido("#paso2");
+});
 
-    console.log(smartphone[2]);
-    console.log(smartphone[3]);
-    preguntaModelo = (prompt("¿Cuál modelo te gustaría comprar y financiar?", "S23 Ultra") || "").toUpperCase();
 
-} else{
-    alert("Te mostramos por consola todos los Smartphones disponibles.");
 
-    for(let i = 0; i < smartphone.length; i++){
-       console.log(smartphone[i]);
-    }
-    preguntaModelo = (prompt("¿Cuál modelo te gustaría comprar y financiar?", "Iphone 14 Pro") || "").toUpperCase();
+//Aqui esta la funcion para crear cada card de producto
+const galeriaDeProductos = document.querySelector("#seleccionarModelo");
+const arrayDeProductosAgregados = JSON.parse(localStorage.getItem("productosAgregados")) || [];
+const itemsEnIconoCarrito = document.querySelector("#cantidadItemsCarrito");
+actualizarCantidadCarrito ()
+
+
+
+//En este condicional indicamos que si hay productos en el carrito, cuando el usuario vuelva a ingresar pueda ver los botones de tarjetas para no tener que seleccionar el telefono de nuevo, ya que lo tiene guardado en carrito.
+if(arrayDeProductosAgregados.length > 0){
+    mostrarContenido("#seleccionarTarjeta");
 }
 
 
-const precioIphone14 = 472000;
-const precioIphone14Pro = 734405;
-const precioS23 = 319999;
-const precioS23Ultra = 459999;
+//En esta funcion creamos cada producto
+function crearProductos(smartphone){
+    galeriaDeProductos.innerHTML = "";
+    
+    smartphone.forEach((smartphone) => {
+    
+        const cadaProducto = document.createElement("div");
+        cadaProducto.className = "cadaCard";
+        
+        cadaProducto.innerHTML = `
+            <img src="${smartphone.img}" class="cardImagen" alt="${smartphone.modelo}">
+            <div class="cardTextos">
+                <p class="cardMarca">${smartphone.marca}</p>
+                <p class="cardTitulo">${smartphone.modelo}</p>
+                <p class="cardDetalle">Memoria: ${smartphone.memoria}</p>
+                <p class="cardDetalle">Color: ${smartphone.color}</p>
+                <p class="cardPrecio">$${smartphone.precio}</p>
+                
+        </div>`;
 
+            const botonAgregar = document.createElement("button");
+            botonAgregar.innerText = "AGREGAR"
+            cadaProducto.appendChild(botonAgregar);
+            botonAgregar.className = "botonAgregar";
 
-const precios = [
-    {modelo: "Iphone 14", precio: precioIphone14},
-    {modelo: "Iphone 14 Pro", precio: precioIphone14Pro},
-    {modelo: "S23", precio: precioS23},
-    {modelo: "S23 Ultra", precio: precioS23Ultra},
-];
+            galeriaDeProductos.appendChild(cadaProducto);
 
+            
+            botonAgregar.addEventListener("click", () => {
+                if(arrayDeProductosAgregados.some((producto) => producto.id === smartphone.id)){
+                    let indice = arrayDeProductosAgregados.findIndex((producto) => producto.id === smartphone.id);
+                    arrayDeProductosAgregados[indice].cantidad++;
+                }else{
+                    smartphone.cantidad = 1;
+                    arrayDeProductosAgregados.push(smartphone);
+                }
 
-let precioSmartphoneElegido = 0;
+                if(cuotaSeleccionada !== null){
+                    actualizarResultadosFinanciacion()
+                }
 
+                actualizarCantidadCarrito()
+                
+                localStorage.setItem("productosAgregados", JSON.stringify(arrayDeProductosAgregados));
 
-if((preguntaModelo == "IPHONE 14 PRO")||(preguntaModelo == "IPHONE14 PRO")||(preguntaModelo == "IPHONE14PRO")){
-    alert(`El precio es $${precioIphone14Pro}`);
-    precioSmartphoneElegido += precioIphone14Pro;
-}
-else if((preguntaModelo == "IPHONE 14")||(preguntaModelo == "IPHONE14")){
-    alert(`El precio es $${precioIphone14}`);
-    precioSmartphoneElegido += precioIphone14;
-}
-else if((preguntaModelo == "S23 ULTRA")||(preguntaModelo == "S23ULTRA")||(preguntaModelo == "S 23 ULTRA")){
-    alert(`El precio es $${precioS23Ultra}`);
-    precioSmartphoneElegido += precioS23Ultra;
-}
-else if((preguntaModelo == "S23")||(preguntaModelo == "S 23")){
-    alert(`El precio es $${precioS23}`);
-    precioSmartphoneElegido += precioS23;
-}
-else{
-    alert("Te mostramos por consola los precios de todos los Smartphones disponibles.");
-    precios.forEach((precio) => {
-        console.log(precio);
+                mostrarContenido("#seleccionarTarjeta");
+            });
     });
-}   
+
+};
 
 
-const tarjetas = [
-    {tarjeta: "VISA", cuotas: "1, 3 y 6 sin interés"},
-    {tarjeta: "MASTERCARD", cuotas: "1, 3, 6 y 12 sin interés"}
+
+//Funciones mostrar y ocultar contenido del HTML
+function mostrarContenido(id){
+    document.querySelector(id).className="";
+}
+
+
+mostrarApplePorDefecto()
+
+
+
+//Este es el array de cuotas disponibles para cada tarjeta
+const cuotas = [
+    { 
+        id:1,
+        tipo:"VISA",
+        cuotas: 1,
+    },
+    { 
+        id:2,
+        tipo:"VISA",
+        cuotas: 3,
+    },
+    { 
+        id:3,
+        tipo:"VISA",
+        cuotas: 6,
+    },
+    { 
+        id:4,
+        tipo:"MASTERCARD",
+        cuotas: 1,
+    },
+    { 
+        id:5,
+        tipo:"MASTERCARD",
+        cuotas: 12,
+    },
 ];
 
 
-let preguntaTarjeta = (prompt("¿Con qué tarjeta vas a pagar? Opciones disponibles: Visa o Mastercard", "Visa") || "").toUpperCase();
-alert(`Te mostramos en la consola las cuotas sin interés disponibles para pagos con ${preguntaTarjeta}`);
+
+//Aqui estan las acciones asignadas a cada boton que filtra por marca
+let mostrarCuotasVisa = document.querySelector("#botonVisa");
+mostrarCuotasVisa.addEventListener("click", () => {
+    const cuotasParaVisa = cuotas.filter(producto => producto.tipo === "VISA");
+    crearBotonesCuotas(cuotasParaVisa);
+    mostrarContenido("#paso4");
+});
+
+mostrarApplePorDefecto()
+
+let mostrarCuotasMastercard = document.querySelector("#botonMastercard");
+mostrarCuotasMastercard.addEventListener("click", () => {
+    const cuotasParaMastercard = cuotas.filter(producto => producto.tipo === "MASTERCARD");
+    crearBotonesCuotas(cuotasParaMastercard);
+    mostrarContenido("#paso4");
+});
 
 
-let montoTotal = precioSmartphoneElegido;
-let preguntaCuotas;
-const cuotasVisa = [1, 3, 6];
-const cuotasMastercard = [1, 3, 6, 12];
-let cadaCuota;
+
+//Aqui esta la funcion para la seleccion de cuotas
+const contenedorOpcionesCuotas = document.querySelector("#seleccionarCuotas");
+const contenedorResultadoFinanciacion = document.querySelector("#resultadoFinanciacion");
+
+let cuotaSeleccionada = null;
+let tarjetaSeleccionada = null;
+
+function crearBotonesCuotas(cuotas){
+    contenedorOpcionesCuotas.innerHTML = "";
+    contenedorResultadoFinanciacion.innerHTML = "";
+
+    cuotas.forEach((cuotas) => {
+    
+        const cadaCuotaDisponible = document.createElement("div");
+        cadaCuotaDisponible.innerHTML = `
+
+        <button id="${cuotas.cuotas}" type="button" class="botonCuota"> ${cuotas.cuotas} cuotas sin interés con ${cuotas.tipo} </button>
+        `;
+        contenedorOpcionesCuotas.appendChild(cadaCuotaDisponible);
+
+        const botonCuota = document.getElementById(`${cuotas.cuotas}`);
+        botonCuota.addEventListener("click", () => {
+            cuotaSeleccionada = botonCuota.id;
+            tarjetaSeleccionada = cuotas.tipo;
+            actualizarResultadosFinanciacion ()
+        });
+    });
+
+};
 
 
-function forDentroDeElse (tipoTarjeta){
-    for (const todosLosPrecios of precios) {
 
-        for(let i = 0; i < tipoTarjeta.length; i++){
-            cadaCuota = tipoTarjeta[i];
-            console.log(`El precio del ${todosLosPrecios.modelo} en ${cadaCuota} cuota/s es $${(todosLosPrecios.precio/cadaCuota)}`);
-        }
+//En esta funcion creamos el detalle de los resultados de financiacion
+function actualizarResultadosFinanciacion (){
+    
+    contenedorResultadoFinanciacion.innerHTML = "";
+    
+    mostrarTituloResultadoFinanciacion()
+
+    for (const smartphone of arrayDeProductosAgregados) {
+
+        const contenedorDatosSmartphone = document.createElement("div");
+        contenedorDatosSmartphone.className = ".contenedorDatosSmartphone"
+        contenedorDatosSmartphone.innerHTML = `
+
+            <div class"datosSmartphone">
+                <div class="datoEncabezado">
+                    <p class="datoModelo"> ${smartphone.modelo}</p>
+
+                    <button id="eliminar-${smartphone.id}"  class="iconoTacho" type="button"><img src="./Multimedia/tacho.svg" alt="icono-tacho"></button>
+                </div>
+
+                <div>
+                    <p class="datoCantidad">Cantidad: ${smartphone.cantidad}</p>
+                    <p class="datoPrecioUnitario">Precio unitario: $${smartphone.precio}</p>
+                </div>  
+            </div>
+            `;
+
+            contenedorResultadoFinanciacion.appendChild(contenedorDatosSmartphone);
+
+            const botonTacho = document.getElementById(`eliminar-${smartphone.id}`);
+            botonTacho.addEventListener("click", () => {
+                eliminarItems (smartphone.id);
+            });
+            
+            const resultadoFinanciacion = smartphone.precio * smartphone.cantidad / cuotaSeleccionada;
+
+            const detalleFinanciacion = document.createElement("div");
+            detalleFinanciacion.innerHTML = `
+
+                <div id="resultadoFinanciacion">
+                    <p class="cuotasParaFinanciar"> ${cuotaSeleccionada} cuotas sin interés de $${resultadoFinanciacion}</p>
+                    <div class="lineaDivisora"></div>
+                </div>`;
+
+            contenedorDatosSmartphone.appendChild(detalleFinanciacion);
+
     }
+
 }
 
-function dentroDeElse(tipoTarjeta){
-    if(preguntaModelo != ""){
-        for (const todasLasCuotas of tipoTarjeta) {
-            console.log(`El precio en ${todasLasCuotas} cuota/s es $${(montoTotal/todasLasCuotas)}`);
-        }
-        console.log("Además, si llevás el smartphone en 1 cuota, tenés 25% de descuento");
+
+
+//En esta funcion actualizamos el numero de items agregados en el icono carrito
+function actualizarCantidadCarrito (){
+    let cantidad = 0;
+                
+    for(let i = 0; i < arrayDeProductosAgregados.length; i++){
+        cantidad += arrayDeProductosAgregados[i].cantidad;
+        
+    }
+    itemsEnIconoCarrito.innerHTML = cantidad;
+}
+
+
+
+//En esta funcion eliminamos items o productos completos de los resultados de financiacion
+function eliminarItems (id){
+    arrayDeProductosAgregados.some((producto) => producto.id === id);
+
+    let indice = arrayDeProductosAgregados.findIndex((producto) => producto.id === id);
+
+    if(arrayDeProductosAgregados[indice].cantidad > 1){
+        arrayDeProductosAgregados[indice].cantidad--;
+
     }else{
-        alert("Si no pudiste elegir un smartphone o la forma de pago, te brindamos en consola todos los precios y su financiación.");
-        
-        forDentroDeElse (tipoTarjeta);
-        
-        console.log("Además, si llevás el smartphone en 1 cuota, tenés 25% de descuento");
+        arrayDeProductosAgregados.splice(indice, 1);
     }
+    actualizarCantidadCarrito()
+    actualizarResultadosFinanciacion ()
+
+    localStorage.setItem("productosAgregados", JSON.stringify(arrayDeProductosAgregados));
 }
 
 
 
-if(preguntaTarjeta == "VISA"){
-    console.log(tarjetas.find((tarjeta) => tarjeta.tarjeta === preguntaTarjeta));
+//En esta funcion mostramos los productos apple por default al principio
+function mostrarApplePorDefecto(){
+    document.querySelector("#botonMarcaApple");
+    let productosApple = smartphone.filter(producto => producto.marca === "Apple");
 
-    preguntaCuotas = parseInt(prompt("¿En cuántas cuotas te gustaría financiar? Ingresá un número"));
+    mostrarProductosApple = crearProductos(productosApple);
+    mostrarContenido("#paso2");
+};
+
+
+
+//En esta funcion mostramos el titulo del bloque de resultados despues de que el usuario hace click en las cuotas
+function mostrarTituloResultadoFinanciacion(){
     
-    function calculoPagos(tipoTarjeta,cantidadCuotas){
+    const tituloResultado = document.createElement("div");
+    tituloResultado.innerHTML =`
 
-        if((preguntaModelo != "") && (cantidadCuotas == tipoTarjeta.find((cuotas) => cuotas === 1))){
-            montoFinanciado = (montoTotal * 0.75);
-            alert(`${nombre}, si llevas el ${preguntaModelo} en ${cantidadCuotas} cuota, tenés 25% de descuento y pagarías $${montoFinanciado}`);
-
-        }else if((preguntaModelo != "") &&((cantidadCuotas === 3)||(cantidadCuotas === 6))){
-            montoFinanciado = (montoTotal/cantidadCuotas);
-            alert(`${nombre}, si llevas el ${preguntaModelo} en ${cantidadCuotas} cuotas, cada cuota te queda en $${montoFinanciado}`);
-        }
-        else{
-            dentroDeElse(cuotasVisa);
-        }
-    };
-    calculoPagos(cuotasVisa,preguntaCuotas);
-
-    
-}else if(preguntaTarjeta == "MASTERCARD"){
-    console.log(tarjetas.find((tarjeta) => tarjeta.tarjeta === preguntaTarjeta));
-
-    preguntaCuotas = (parseInt(prompt("¿En cuántas cuotas te gustaría financiar? Ingresá un número")) || "");
-
-    function calculoPagos(tipoTarjeta,cantidadCuotas){
-
-        if((preguntaModelo != "") && (cantidadCuotas == tipoTarjeta.find((cuotas) => cuotas === 1))){
-            montoFinanciado = (montoTotal * 0.75);
-            alert(`${nombre}, si llevas el ${preguntaModelo} en ${cantidadCuotas} cuota, tenés 25% de descuento y pagarías $${montoFinanciado}`);
-        }
-        else if((preguntaModelo != "") && ((cantidadCuotas === 3)||(cantidadCuotas === 6)||(cantidadCuotas === 12))){
-            montoFinanciado = (montoTotal/cantidadCuotas);
-            alert(`${nombre}, si llevas el ${preguntaModelo} en ${cantidadCuotas} cuotas, cada cuota te queda en $${montoFinanciado}`);
-        }
-        else{
-            dentroDeElse(cuotasMastercard);
-        }
-    };
-    calculoPagos(cuotasMastercard,preguntaCuotas);
+    <div id="contenedorTituloResultado">
+        <img src="./Multimedia/tarjeta.svg" alt="icono-tarjeta" class="iconoTarjeta">
+        <h2 id="titulo">Te mostramos la financiación con ${tarjetaSeleccionada}</h2>
+    <div>
+    `;
+    contenedorResultadoFinanciacion.appendChild(tituloResultado);
 }
-else{
-    tarjetas.forEach((tarjeta) => {
-        console.log(tarjeta);
-    });
-
-    alert("Si no pudiste elegir un smartphone o la forma de pago, te brindamos en consola todos los precios y su financiación.");
-
-    forDentroDeElse(cuotasMastercard);
-
-    console.log("Si llevás el smartphone en 1 cuota, tenés 25% de descuento");
-}
-
-
-
-
-
-// Tienen que almacenar datos en el storage y recuperar esos datos
-// setItem y getItem
-// Ojo datos relacionados con el proyecto
-// Si tienen un carrito de compras deben tener los objetos en un json
-//nada de alert y prompt
-//almacenar en storage e imprimir los datos en pantalla, los datos guardados por storage
-
-
-find
